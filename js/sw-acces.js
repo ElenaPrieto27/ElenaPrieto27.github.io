deb4ee11f3f499f3a74ee7b6f7ce7176f752b3a1
¨ElenaPrieto27¨ <elenaprieto.c59@gmail.com>
2022-04-07 18:17:22 -0500

    dinamic cache

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -4,8 +4,10 @@ function actualizaCacheDinamico(dynamicCache, request, response){
         //la respuesta tiene data y se debe almacaenar en cache
         return caches.open(dynamicCache).then(cache => {
             cache.put(request,response.clone() );
-            return cache.clone();
+            
         });
+
+        return dynamicCache.clone();
     }else{//si no viene nda en la respuesta
         return response;
     }
