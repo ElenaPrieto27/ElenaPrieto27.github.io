85a6f9154961129d9a55e7866a4dd1aa3e2fbdd3
¨ElenaPrieto27¨ <elenaprieto.c59@gmail.com>
2022-04-07 20:13:23 -0500

    elebae

--- sw.js
+++ sw.js
@@ -1,47 +1,35 @@
-//Archivo sw.js
-//Agregar la referencia al archivo js/sw-acceces.js
-
 importScripts('js/sw-acces.js');
 
-//Archivo sw.js
-//1. Determinar el contenido de los caches de la PWA
+const myUrl = 'https://elenaprieto27.github.io/';
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
-
 const APP_SHELL = [
-    '/',
-    'index.html',
-    'css/style.css',
-    //'images/avs/img1.jpg',
+    '/', 
+    'index.html', 
+    'css/style.css', 
     'js/app.js'
 ];
-const APP_SHELL_INMUTABLE = [   
+const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    'js/libs/jquery.js',
-    'http://www.croop.cl/UI/twitter/images/carl.jpg',
-    'http://www.croop.cl/UI/twitter/images/russel.jpg',
-    'http://www.croop.cl/UI/twitter/images/doug.jpg'
-];
+    'https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css', 
+    'js/libs/jquery.js'];
 
-//Archivo sw.js
-//2. Hacer la instalación del sw, cargando los caches.
+self.addEventListener('install', event =>{
+    const cacheApp = caches.open(STATIC_CACHE).then(cache =>{
+        cache.addAll(APP_SHELL);
+    });
 
-self.addEventListener('install', event => {
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache => {
-         cache.addAll(APP_SHELL);
-     });
-     const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
-         cache.addAll(APP_SHELL_INMUTABLE);
-     });
-     event.waitUntil(Promise.all([cacheStatic,cacheInmutable]));
- });
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
+        cache.addAll(APP_SHELL_INMUTABLE);
+    });
 
- //Archivo sw.js
-//3. Hacer la activación del sw, eliminando las versiones antiguas de cache.
+    event.waitUntil(Promise.all([cacheApp, cacheInmutable]));
+});
 
+	
 self.addEventListener('activate', event => {
     const respuesta = caches.keys().then(keys =>{
         keys.forEach(key => {
@@ -53,17 +41,11 @@ self.addEventListener('activate', event => {
     event.waitUntil(respuesta);
 });
 
-//Archivo sw.js
-//3. Hacer la estrategia de cache, este ejemplo un marco referencial, cada equipo determinará 
-//la estrategia adecuada para su sitio web, esto conforme a las estrategias trabajadas en clase.
-
 self.addEventListener('fetch', event => {
     const respuesta = caches.match(event.request).then( res => {
         if(res){return res;}
         else{
             return fetch(event.request).then(newRes => {
-                //Agregar en el directorio /js un archivo llamdado sw-acces.js
-                //y programar la funcion actualizaCacheDinamico, para tener mas limpio el proyecto.
                 return actualizaCacheDinamico(DYNAMIC_CACHE, event.request, newRes);
             });
         }
