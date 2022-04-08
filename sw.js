a7e0d0d7d9336cde5d0fd6138b3bbfba5e7ad4ff
¨ElenaPrieto27¨ <elenaprieto.c59@gmail.com>
2022-04-07 20:07:31 -0500

    NO DIAGONALES X3 >:C

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    'libs/jquery.js',
+    'js/libs/jquery.js',
     'http://www.croop.cl/UI/twitter/images/carl.jpg',
     'http://www.croop.cl/UI/twitter/images/russel.jpg',
     'http://www.croop.cl/UI/twitter/images/doug.jpg'
